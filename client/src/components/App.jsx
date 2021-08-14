88c09cd47bc7febaf563af913a5d44b2cbd9982f
Gareth Rogers <garethdavisrogers@gmail.com>
2021-08-14 16:00:08 -0500

    search is now functional

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,14 +1,67 @@
 import React from "react";
+import axios from "axios";
+import Students from "./Students";
 
 class App extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      students: null,
+      filterStudents: null,
+      search: null,
+    };
+  }
+
+  handleGetFields = (e) => {
+    const { search, students } = this.state;
+    this.setState({ search: e.target.value });
+    if (search === "") {
+      this.setState({ filterStudents: students });
+    } else {
+      let filter = [];
+      students.forEach((student) => {
+        if (
+          student.firstName.toLowerCase().includes(search) ||
+          student.lastName.toLowerCase().includes(search)
+        ) {
+          filter.push(student);
+        }
+      });
+      this.setState({ filterStudents: filter });
+    }
+  };
+
+  componentDidMount() {
+    axios
+      .get("https://api.hatchways.io/assessment/students")
+      .then((response) =>
+        this.setState({
+          students: response.data.students,
+          filterStudents: response.data.students,
+        })
+      )
+      .catch((err) => {
+        console.error(err);
+      });
   }
 
   render() {
-    return <div>Hello World</div>;
+    const { students, search, filterStudents } = this.state;
+    return (
+      <div>
+        <div className="search-container">
+          <input
+            type="text"
+            placeholder="Search by name"
+            onChange={this.handleGetFields}
+          ></input>
+        </div>
+        {filterStudents !== null && (
+          <Students search={search} filterStudents={filterStudents} />
+        )}
+      </div>
+    );
   }
 }
 
